4e3e18f9780dd55603bc9e3267869c0725bfa19c
workmgh-netlify <43612814+workmgh-netlify@users.noreply.github.com>
2018-09-26 17:03:39 +0200

    Update Banner.js

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -4,10 +4,10 @@ const Banner = (props) => (
     <section id="banner" className="major">
         <div className="inner">
             <header className="major">
-                <h1>Biodiversity of Togo</h1>
+                <h1>Biodiversity of Tecala</h1>
             </header>
             <div className="content">
-                <p>The Atlas of Living Togo allows you to the biodiversity of Togo</p>
+                <p>The Atlas of Living Togo allows you to the biodiversity of Tecala</p>
                 <ul className="actions">
                     <li><a href="/occurrences" className="button next scrolly">Start exploring</a></li>
                 </ul>
